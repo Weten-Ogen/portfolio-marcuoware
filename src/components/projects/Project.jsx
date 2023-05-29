ce524fcc5b04fc8ca10f3b7617263bbd8d1b99cc
Weten-Ogen <alphaoware@gmail.com>
2023-05-29 11:58:57 +0000

    fixed the project

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 
-const Project = (props) => {
+export const Project = (props) => {
   const{title,stack,image} = props.item
 
   return (
