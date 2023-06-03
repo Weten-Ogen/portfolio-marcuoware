0ef03e3ec6deee6bba1260c7dcbd00c30e90b654
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-03 12:33:57 +0000

    Update projects.js

--- data/projects.js
+++ data/projects.js
@@ -5,27 +5,15 @@ export const projectsData=[
         "stack":'react,firebase,tailwind',
         "image":'/assets/projects/socio.png',
     },
+    
     {
         "id":'2',
-        "title":'salina cateen',
-        "stack":'react ,tailwind',
-        "image":'/assets/projects/restaurant.png',
-
-    },
-    {
-        "id":'3',
         "title":'next-dashboard',
         "stack":'next,tailwind,ChartJs',
         "image":'/assets/projects/dashboard.png',
 
     },
-    {
-        "id":'4',
-        "title":'sneak ecomerce',
-        "stack":'react,tailwind,redux',
-        "image":'/assets/projects/sneakers.png',
-
-    },
+   
 ]
 
 export default projectsData
