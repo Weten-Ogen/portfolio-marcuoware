a3a2d3bc98f299a3537d5ca10d9412ef5437b9ab
Marcus G. Oware <90935734+Weten-Ogen@users.noreply.github.com>
2023-06-04 07:11:14 +0000

    Update restaurant.jsx

--- pages/restaurant.jsx
+++ pages/restaurant.jsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import React from 'react';
-import restaurantImg from '../public/assets/projects/restaurant.png';
+import dashboardImg from '../public/assets/projects/dashboard.png';
 import { RiRadioButtonFill } from 'react-icons/ri';
 import Link from 'next/link';
 
-const restaurant = () => {
+const dashboard = () => {
   return (
     <div className='w-full'>
       <div className='w-screen h-[50vh] relative'>
@@ -13,7 +13,7 @@ const restaurant = () => {
           className='absolute z-1'
           layout='fill'
           objectFit='cover'
-          src={restaurantImg}
+          src={dashboardImg}
           alt='/'
         />
         <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2'>
@@ -71,4 +71,4 @@ const restaurant = () => {
   );
 };
 
-export default restaurant;
+export default dashboard;
